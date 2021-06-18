fa16bfb520668af27e987493554951a84a5fa86e
Muhammad Umar <ee_umar@yahoo.com>
2021-06-18 11:03:03 +0100

    Initial Commit

---
+++ iOSArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSArchitecture
+//
+//  Created by Muhammad Umar on 18/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
